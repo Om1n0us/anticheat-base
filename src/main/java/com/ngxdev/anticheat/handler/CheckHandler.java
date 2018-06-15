e1dd7ba5d83c94befe00a51e3bb78fcfd74c0eee
lukek <luke.kander@gmail.com>
2018-06-15 22:46:42 +0300

    Don't init noop checks (woops)

--- src/main/java/com/ngxdev/anticheat/handler/CheckHandler.java
+++ src/main/java/com/ngxdev/anticheat/handler/CheckHandler.java
@@ -12,24 +12,35 @@ import com.ngxdev.anticheat.data.PlayerData;
 import com.ngxdev.utils.ClassScanner;
 import com.ngxdev.utils.Init;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Init
 public class CheckHandler {
     private static List<Class<? extends Check>> checks = new ArrayList<>();
+    private static List<Class<? extends Check>> parsers = new ArrayList<>();
     private static Map<Class<? extends Check>, CheckWrapper> wrappers = new HashMap<>();
 
     public CheckHandler() {
-        Arrays.asList(ClassScanner.scanFile(CheckType.class.getName(), getClass()),
-                ClassScanner.scanFile(NoOpCheck.class.getName(), getClass()))
-                .forEach(list -> list.forEach(c -> {
-                    try {
-                        // Check casts
-                        checks.add((Class<? extends Check>) Class.forName(c));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }));
+        ClassScanner.scanFile(CheckType.class.getName(), getClass()).forEach(c -> {
+            try {
+                // Check casts
+                checks.add((Class<? extends Check>) Class.forName(c));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        ClassScanner.scanFile(NoOpCheck.class.getName(), getClass()).forEach(c -> {
+            try {
+                // Check casts
+                parsers.add((Class<? extends Check>) Class.forName(c));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     public static void init(PlayerData data) {
@@ -37,6 +48,15 @@ public class CheckHandler {
             try {
                 Check check = c.newInstance();
                 check.init(data, wrappers.computeIfAbsent(c, CheckWrapper::new));
+                data.checks.add(check);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        parsers.forEach(c -> {
+            try {
+                Check check = c.newInstance();
                 data.checks.add(check);
             } catch (Exception e) {
                 e.printStackTrace();
