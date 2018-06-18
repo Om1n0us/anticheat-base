2bbc1d2c5804fc702a404dd12fa1eabae286f43c
lukek <luke.kander@gmail.com>
2018-06-18 03:51:51 +0300

    Finishing up Collision Handler

--- src/main/java/com/ngxdev/anticheat/world/CollisionHandler.java
+++ src/main/java/com/ngxdev/anticheat/world/CollisionHandler.java
@@ -1,7 +1,12 @@
 package com.ngxdev.anticheat.world;
 
 import com.ngxdev.anticheat.containers.basic.SimpleLocation;
+import com.ngxdev.anticheat.handler.TinyProtocolHandler;
+import com.ngxdev.utils.Materials;
 import lombok.Getter;
+import net.minecraft.server.v1_8_R3.EnumParticle;
+import net.minecraft.server.v1_8_R3.PacketPlayOutWorldParticles;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -9,16 +14,15 @@ import org.bukkit.block.BlockState;
 import org.bukkit.material.Door;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static org.bukkit.Material.*;
 
+@Getter
 public class CollisionHandler {
     private List<Block> blocks;
     private SimpleLocation location;
-    private SimpleCollisionBox box;
     private double width, height;
     private double shift;
 
@@ -37,15 +41,39 @@ public class CollisionHandler {
         this.shift = shift;
     }
 
+
     public boolean isCollidedWith(int bitmask) {
-        return false;
+        SimpleCollisionBox playerBox = new SimpleCollisionBox()
+                .offset(location.getX(), location.getY(), location.getZ())
+                .offset(0, shift, 0)
+                .expandMax(0, height, 0)
+                .expand(width / 2, 0, width / 2);
+
+        for (double x = playerBox.x1; x < playerBox.x2; x += 0.2) {
+            for (double y = playerBox.y1; y < playerBox.y2; y += 0.2) {
+                for (double z = playerBox.z1; z < playerBox.z2; z += 0.2) {
+                    final double fX = x;
+                    final double fY = y;
+                    final double fZ = z;
+                    Bukkit.getOnlinePlayers().forEach(p -> {
+                        TinyProtocolHandler.sendPacket(p, new PacketPlayOutWorldParticles(EnumParticle.ENCHANTMENT_TABLE, true, (float) fX, (float) fY, (float) fZ, 0F, 0F, 0F, 0, 1));
+                    });
+                }
+            }
+        }
+
+        return blocks.stream()
+                .filter(b -> Materials.checkFlag(b.getType(), bitmask))
+                .anyMatch(b -> {
+                    return BlockData.getData(b.getType()).getBox().isCollided(location, playerBox, b);
+                });
     }
 
     enum BlockData {
         LIQUID(new SimpleCollisionBox(0, 0, 0, 1, 0.9, 0),
                 WATER, LAVA, STATIONARY_LAVA, STATIONARY_WATER),
         BREWINGSTAND(new ComplexCollisionBox(
-                new SimpleCollisionBox(0.1, 0.0, 0.1, 0.9, 0.1, 0.9),              //base
+                new SimpleCollisionBox(0, 0.0, 0, 1, 0.1, 1),                      //base
                 new SimpleCollisionBox(0.4375, 0.0, 0.4375, 0.5625, 0.875, 0.5625) //top
         ), BREWING_STAND),
         DOOR(new DynamicCollisionBox(b -> {
@@ -55,7 +83,11 @@ public class CollisionHandler {
             //todo;
             return new SimpleCollisionBox(0, 0, 0, 1, 1, 0.2);
         }), WOOD_DOOR, ACACIA_DOOR, BIRCH_DOOR, JUNGLE_DOOR, IRON_DOOR, DARK_OAK_DOOR, SPRUCE_DOOR),
-        DEFAULT(new SimpleCollisionBox(0, 0, 0, 1, 1 ,1));
+        CARPET(new SimpleCollisionBox(0.0F, 0.0F, 0.0F, 1.0F, 0.0625F, 1.0F),
+                Material.CARPET),
+        LILIPAD(new SimpleCollisionBox(0.0f, 0.0F, 0.0f, 1.0f, 0.015625F, 1.0f),
+                Material.WATER_LILY),
+        DEFAULT(new SimpleCollisionBox(0, 0, 0, 1, 1, 1));
 
         @Getter
         private CollisionBox box;
@@ -75,11 +107,15 @@ public class CollisionHandler {
     }
 
     interface CollisionBox {
-        boolean isCollided(CollisionBox other, Block block);
+        boolean isCollided(SimpleLocation location, CollisionBox other, Block block);
     }
 
     static class SimpleCollisionBox implements CollisionBox {
         double x1, y1, z1, x2, y2, z2;
+
+        public SimpleCollisionBox() {
+            this(0, 0, 0, 0, 0, 0);
+        }
 
         public SimpleCollisionBox(double x1, double y1, double z1, double x2, double y2, double z2) {
             this.x1 = x1;
@@ -90,18 +126,66 @@ public class CollisionHandler {
             this.z2 = z2;
         }
 
-        @Override
-        public boolean isCollided(CollisionBox other, Block block) {
-            if (other instanceof SimpleCollisionBox) {
-                SimpleCollisionBox box = (SimpleCollisionBox) other;
-                return (box.x2 > this.x1 && box.x1 < this.x2)
-                        && ((box.y2 > this.y1 && box.y1 < this.y2)
-                        && (box.z2 > this.z1 && box.z1 < this.z2));
-            } else throw new IllegalStateException("Attempted to check collision with " + other.getClass().getSimpleName());
+        public SimpleCollisionBox copy() {
+            return new SimpleCollisionBox(x1, y1, z1, x2, y2, z2);
         }
 
-        private boolean contains(double min, double max, double value) {
-            return min < value && max > value;
+        public SimpleCollisionBox offset(double x, double y, double z) {
+            this.x1 += x;
+            this.y1 += y;
+            this.z1 += z;
+            this.x2 += x;
+            this.y2 += y;
+            this.z2 += z;
+            return this;
+        }
+
+        public SimpleCollisionBox offsetMin(double x, double y, double z) {
+            this.x1 += x;
+            this.y1 += y;
+            this.z1 += z;
+            return this;
+        }
+
+        public SimpleCollisionBox expandMax(double x, double y, double z) {
+            this.x2 += x;
+            this.y2 += y;
+            this.z2 += z;
+            return this;
+        }
+
+        public SimpleCollisionBox expand(double x, double y, double z) {
+            this.x1 -= x;
+            this.y1 -= y;
+            this.z1 -= z;
+            this.x2 += x;
+            this.y2 += y;
+            this.z2 += z;
+            return this;
+        }
+
+        @Override
+        public boolean isCollided(SimpleLocation location, CollisionBox other, Block block) {
+            if (other instanceof SimpleCollisionBox) {
+                SimpleCollisionBox box = ((SimpleCollisionBox) other);
+                SimpleCollisionBox blockBox = copy().offset(block.getX(), block.getY(), block.getZ());
+                boolean collided = (box.x2 > blockBox.x1 && box.x1 < blockBox.x2)
+                        && ((box.y2 > blockBox.y1 && box.y1 < blockBox.y2)
+                        && (box.z2 > blockBox.z1 && box.z1 < blockBox.z2));
+                for (double x = blockBox.x1; x < blockBox.x2; x += 0.241) {
+                    for (double y = blockBox.y1; y < (blockBox.y2 < 0.1 ? 0.1 : blockBox.y2); y += 0.241) {
+                        for (double z = blockBox.z1; z < blockBox.z2; z += 0.241) {
+                            final double fX = x;
+                            final double fY = y;
+                            final double fZ = z;
+                            Bukkit.getOnlinePlayers().forEach(p -> {
+                                TinyProtocolHandler.sendPacket(p, new PacketPlayOutWorldParticles(collided ? EnumParticle.FLAME : EnumParticle.CRIT, true, (float) fX, (float) fY, (float) fZ, 0F, 0F, 0F, 0, 1));
+                            });
+                        }
+                    }
+                }
+                return collided;
+            } else throw new IllegalStateException("Attempted to check collision with " + other.getClass().getSimpleName());
         }
     }
 
@@ -113,8 +197,8 @@ public class CollisionHandler {
         }
 
         @Override
-        public boolean isCollided(CollisionBox other, Block block) {
-            return boxes.stream().anyMatch(box -> box.isCollided(other, block));
+        public boolean isCollided(SimpleLocation location, CollisionBox other, Block block) {
+            return boxes.stream().anyMatch(box -> box.isCollided(location, other, block));
         }
     }
 
@@ -126,8 +210,8 @@ public class CollisionHandler {
         }
 
         @Override
-        public boolean isCollided(CollisionBox other, Block block) {
-            return box.fetch(block).isCollided(other, block);
+        public boolean isCollided(SimpleLocation location, CollisionBox other, Block block) {
+            return box.fetch(block).isCollided(location, other, block);
         }
 
         interface DynamicBox {
